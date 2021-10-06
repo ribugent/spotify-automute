2ca0574d5043eb924fa5ca9d26d6f47415b90201
Gerard Ribugent <ribugent@gmail.com>
2021-10-06 13:25:21 +0200

    First version of spotify automuter

---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import dbus
+import re
+import pulsectl
+
+session_bus = dbus.SessionBus()
+pulse = pulsectl.Pulse('spotify-automute')
+
+
+def main():
+	spotify_track_id = get_spotify_track_id()
+	spotify_sinks = get_spotify_sinks()
+	is_ad = track_is_ad(spotify_track_id)
+	sinks_mute(spotify_sinks, is_ad)
+
+
+def get_spotify_track_id():
+	spotify_bus = session_bus.get_object(
+		"org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+	spotify_properties = dbus.Interface(
+		spotify_bus, "org.freedesktop.DBus.Properties")
+
+	metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+	return metadata["mpris:trackid"]
+
+
+def track_is_ad(track_id):
+	return re.match(r"^spotify:ad:", track_id) != None
+
+
+def get_spotify_sinks():
+	input_sinks = pulse.sink_input_list()
+	return filter(lambda sink: sink.proplist.get('application.process.binary'), input_sinks)
+
+
+def sinks_mute(sinks, mute):
+	for sink in sinks:
+		pulse.sink_input_mute(sink.index, mute)
+
+
+main()
